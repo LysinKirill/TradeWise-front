5b3c954cec0be7d88ccaebc3b909ff5fa4ee4dd9
epolyakh <ekaterina.polyakh@domrf.ru>
2025-05-21 19:50:08 +0300

    feat: update navlinks colors

--- src/shared/ui/components/Navigation/styles.ts
+++ src/shared/ui/components/Navigation/styles.ts
@@ -1,4 +1,4 @@
-import { colors } from '@/shared/constants/colors';
+import { chartColors, colors } from '@/shared/constants/colors';
 import { styled } from "styled-components";
 
 export const NavLinks = styled.nav`
@@ -11,21 +11,31 @@ export const NavLink = styled.a`
   color: #fff;
   text-decoration: none;
   font-size: 1rem;
-  font-weight: 700;
+  font-weight: 600;
   padding: 0.5rem 1.5rem;
   border-radius: 16px;
   transition: all 0.3s ease;
   box-shadow: 0 0 0 transparent;
 
   &:hover {
-    background: ${colors.darkPurpleButton};
+    background: linear-gradient(
+        135deg,
+        ${colors.neonPurple} 0%,
+        ${chartColors.secondary} 50%,
+        ${colors.neonPurple} 100%
+    );
     transform: scale(1.05);
     box-shadow: 0 0 15px ${colors.purpleButton};
     z-index: 1;
   }
 
   &.active {
-    background: ${colors.purpleButton};
+    background: linear-gradient(
+        135deg,
+        ${chartColors.primary} 0%,
+        ${chartColors.secondary} 50%,
+        ${chartColors.tertiary} 100%
+    );
     text-shadow: 0 0 10px rgba(255, 255, 255, 0), 0 0 20px rgba(255, 255, 255, 0.6), 0 0 30px rgba(255, 255, 255, 0.4); 
     box-shadow: 0 0 15px ${colors.purpleButton};
     font-weight: 600;
